8fd6e87b9ab8aeef9d568f9a66e7f208603b5ba8
giotolentino <giotolentino1226@gmail.com>
2023-04-07 12:14:55 +0800

    second commit

---
+++ music.php
@@ -0,0 +1,52 @@
+<?php header("Access-Control-Allow-Origin: *");
+
+$music = array (
+    array (
+        "title" => "Buloy",
+        "artist" => "Parokya ni Edgar",
+        "release" => "1996",
+        "producer" => "Robert Javier",
+        "genres" => array("genre_1" => "Pop", "genre_2" => "Holloween", "genre_3" => "Rock", "genre_4" => "Pinoy Rock", "genre_5" => "Alternative Rock"),
+        "image" => ""
+
+    ),
+    array (
+        "title" => "Pangako",
+        "artist" => "Cueshe",
+        "release" => "2010",
+        "producer" => "Tred Monteras II",
+        "genres" => array("genre_1" => "Pinoy Rock", "genre_2" => "Pop", "genre_3" => "Rock", "genre_4" => "Drama", "genre_5" => "Romance"),
+        "image" => ""
+
+    ),
+    array (
+        "title" => "Narda",
+        "artist" => "Kamikazee",
+        "release" => "2006",
+        "producer" => "Eric Perlas",
+        "genres" => array("genre_1" => "Post-Punk Revival", "genre_2" => "Alternative Rock", "genre_3" => "New Rave", "genre_4" => "Acoustic Rock", "genre_5" => "Indie Rock"),
+        "image" => ""
+
+    ),
+    array (
+        "title" => "Salamat",
+        "artist" => "The Dawn",
+        "release" => "1989",
+        "producer" => "Teddy Diaz",
+        "genres" => array("genre_1" => "Rock", "genre_2" => "Pop", "genre_3" => "Indonesian Rock", "genre_4" => "Indonesian Pop", "genre_5" => "Holiday"),
+        "image" => ""
+
+    ),
+    array (
+        "title" => "Magbalik",
+        "artist" => "Callalily",
+        "release" => "2006",
+        "producer" => "Nicanor Abernardo",
+        "genres" => array("genre_1" => "Pop Rock", "genre_2" => "Rock", "genre_3" => "Pop", "genre_4" => "Pinoy Rock", "genre_5" => "Indonesian Rock"),
+        "image" => ""
+
+    )
+)
+
+
+?>
